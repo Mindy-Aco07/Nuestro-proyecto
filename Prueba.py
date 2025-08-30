f47c840a13f3ceaf751a45a0332f2fb527f6d721
Asael312 <AL07135560@tecmilenio.mx>
2025-08-29 18:52:44 -0600

    Add product summary input functionality

---
+++ Prueba.py
@@ -0,0 +1,6 @@
+Resumen_de_productos = []
+Nombre_del_producto = input("Ingrese el nombre del producto: ")
+Precio_del_producto = float(input("Ingrese el precio del producto: "))
+Cantidad_del_producto = int(input("Ingrese la cantidad del producto: "))        
+Resumen_de_productos.append("Producto: "+Nombre_del_producto+", Precio: "+str(Precio_del_producto)+", Cantidad: "+str(Cantidad_del_producto))
+print(Resumen_de_productos)
