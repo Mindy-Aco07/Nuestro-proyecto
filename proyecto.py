696dca8400e596ab1d0d219d07e7bda5682c5b32
Cristna Perez <AL07139298@tecmilenio.mx>
2025-09-25 14:32:57 -0600

    Actualice y mejore/arreglue algunas funciones

--- proyecto.py
+++ proyecto.py
@@ -37,37 +37,33 @@ def MostrarInventario(diccionario):
     print("Agregar el codigo para la parte ""MostrarInventario""")
 
 #Aqui va el codigo para actualizar el inventario
-def ActualizarProducto(diccionario, dato=None, nuevo_stock=None):
+def ActualizarProducto(diccionario, archivo, dato=None, nuevo_stock=None):
     if dato is None:
         dato = input("Ingrese producto a actualizar: ")
-    if BuscarProducto(diccionario,dato):
+    if dato in diccionario:
         try:
             if nuevo_stock is None:
                 nuevo_stock = int(input("Ingrese nuevo stock: "))
             diccionario[dato]['Stock'] = nuevo_stock
             print('Producto actualizado.')
+            guardar_inventario(archivo, diccionario)
             return True
         except ValueError:
             print('El stock debe ser numerico.')
             return False
     else:
         print('No encontrado.')
-        input("¿Deseas agaregarlo como nuevo? (si/no): ")
-        if Opcion.lower() == 'si':
-            AgregarProducto(diccionario)
         return False
   
 #Aqui va el codigo para eliminar un producto
-def EliminarProducto(diccionario):
+def EliminarProducto(diccionario, archivo):
     dato = input("Ingrese producto a eliminar: ")
-    nuevo_dato=None
-    for nombre,datos in diccionario:
-        if dato==nombre or dato==datos['codigo']: 
-            nuevo_dato=nombre
-            diccionario.pop(nuevo_dato)
-            print('"Producto eliminado."')
-    else: 
-        print('Producto no encontrado.')
+    if dato in diccionario:
+        diccionario.pop(dato)
+        print('Producto eliminado')
+        guardar_inventario(archivo, diccionario)
+    else:
+        print('Producto no encontrado')
 
 #Aqui va el codigo para buscar un producto
 def BuscarProducto(diccionario,dato=None):
@@ -81,9 +77,6 @@ def BuscarProducto(diccionario,dato=None):
             encontrado = True
     if not encontrado:
         print('No encontrado.')
-        opcion = input('¿Deseas agregarlo? (si/no): ')
-        if opcion.lower().strip() == 'si':
-            AgregarProducto(diccionario)
     return encontrado
 
 #Aqui va el codigo para el calculo final de precios
